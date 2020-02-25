bfcdc8009f302813e9af137009068d91a746c45e
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-02-25 10:32:17 -0300

    #21 - Preparando nosso projeto para o teste

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_203948) do
+ActiveRecord::Schema.define(version: 2020_02_25_130213) do
+
+  create_table "enemies", force: :cascade do |t|
+    t.string "name"
+    t.integer "power_base"
+    t.integer "power_step"
+    t.integer "level"
+    t.integer "kind"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
